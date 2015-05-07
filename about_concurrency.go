2db6fc2484fd54a126bc4deac40549ba5b8382ca
Davis Koh <koh.davis.0@gmail.com>
2015-05-07 13:41:01 -0400

    about concurrency

--- about_concurrency.go
+++ about_concurrency.go
@@ -10,18 +10,24 @@ func isPrimeNumber(possiblePrime int) bool {
 }
 
 func findPrimeNumbers(channel chan int) {
-	for i := 2; ; /* infinite loop */ i++ {
+	for i := 2; i < 12; i++ {
 		// your code goes here
+		if isPrimeNumber(i) == true {
+			channel <- i
+		}
 
 		assert(i < 100) // i is afraid of heights
 	}
+
+	close(channel)
 }
 
 func aboutConcurrency() {
 	ch := make(chan int)
 
-	assert(__delete_me__) // concurrency can be almost trivial
+	// assert(__delete_me__) // concurrency can be almost trivial
 	// your code goes here
+	go findPrimeNumbers(ch)
 
 	assert(<-ch == 2)
 	assert(<-ch == 3)
